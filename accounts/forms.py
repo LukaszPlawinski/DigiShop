a2f0391752510095c0704c29eaf181e346ef90be
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-08-26 07:05:55 +0000

    created basic registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class user_login_form(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    
+class register_user_form(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
