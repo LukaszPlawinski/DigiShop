8c555d4f25b32adcd93b1e4f243aa70fd015a451
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-08-27 18:59:29 +0000

    created detail.html, list.html files and urls for them

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200,
@@ -10,7 +11,9 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
-        
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+        args=[self.slug])
     def __str__(self):
         return self.name
     
@@ -31,5 +34,8 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+        args=[self.id, self.slug])
     def __str__(self):
         return self.name
