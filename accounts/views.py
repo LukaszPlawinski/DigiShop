4fd01f90661347ff91aa7f6a31925edd188f0d5f
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-01 14:15:45 +0000

    removed index view and redirect login, logout, registration to shop:product_list instead

--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render,redirect,reverse
 from .forms import user_login_form, register_user_form
 from django.contrib import auth
 from django.contrib.auth.models import User
+from shop.views import product_list
 
-# Create your views here.
-def index(request):
-    login_form = user_login_form()
-    return render(request, 'index.html')
-    
+
 def login(request):
     if request.user.is_authenticated:
-        return redirect(reverse('index'))
+        return redirect(reverse('shop:product_list'))
     if request.method == "POST":
         login_form = user_login_form(request.POST)
         if login_form.is_valid():
@@ -18,7 +15,7 @@ def login(request):
                                     password=request.POST['password'])
             if user:
                 auth.login(user=user, request=request)
-                return redirect(reverse('index'))
+                return redirect(reverse('shop:product_list'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
@@ -27,11 +24,11 @@ def login(request):
     
 def logout(request):
     auth.logout(request)
-    return render(request, 'index.html')
+    return redirect(reverse('shop:product_list'))
     
 def registration(request):
     if request.user.is_authenticated:
-        return redirect(reverse('index'))
+        return redirect(reverse('shop:product_list'))
     if request.method == "POST":
         registration_form = register_user_form(request.POST)
         if registration_form.is_valid():
@@ -41,7 +38,7 @@ def registration(request):
                                      password=request.POST['password1'])
             if user:
                 auth.login(user=user, request=request)
-                return redirect(reverse('index'))
+                return redirect(reverse('shop:product_list'))
     else:
         registration_form = register_user_form()
     return render(request, 'registration.html', {
