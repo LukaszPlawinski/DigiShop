b4d8a4f667a79bfd2c1977d5a1ed87d3cbf5a51a
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-02 19:52:23 +0000

    created view_cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-
+app_name = 'cart'
 urlpatterns = [
 	path('view_cart', views.view_cart, name='view_cart'),
-	path('add_to_cart', views.add_to_cart, name='add_to_cart'),
-	path('adjust_cart', views.adjust_cart, name='adjust_cart'),
+
 ]
