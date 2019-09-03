7f5d578ff013f05f013b8d41aac56c8d8d41f886
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-03 22:09:32 +0000

    registered cart session

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cart',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CART_SESSION_ID = 'cart'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -68,7 +69,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.contexts.cart_contents'
             ],
         },
     },
