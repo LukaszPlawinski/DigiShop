6932c66547342b89194bbf0cafb3cab2c61deb23
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-07 12:18:30 +0000

    added stripe payment to view

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.conf import settings
+import stripe
+
+
+stripe.api_key = settings.STRIPE_SECRET
 
 def order_create(request):
     cart = Cart(request)
+    total = int(cart.get_total_price() * 100)
     if request.method == 'POST':
+
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
+            charge = stripe.Charge.create(
+                amount= total,
+                currency='EUR',
+                description= order.id,
+                source=request.POST['stripeToken']
+        )
         for item in cart:
             OrderItem.objects.create(order=order,
                                         product=item['product'],
@@ -21,6 +34,8 @@ def order_create(request):
                             {'order': order})
     else:
         form = OrderCreateForm()
+        key = settings.STRIPE_PUBLISHABLE
     return render(request,
                 'create.html',
-                {'cart': cart, 'form': form})
+                {'cart': cart, 'form': form, 'key':key, 'total':total})
+                
