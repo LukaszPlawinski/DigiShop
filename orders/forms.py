45b5f1b1f458d912d328311eaa3309b7146051df
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-07 09:18:06 +0000

    changed names of the fields

--- orders/forms.py
+++ orders/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from .models import Order
+from django.contrib.auth.models import User
 
 class OrderCreateForm(forms.ModelForm):
     
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address',
+        fields = ['first_name','last_name','email','address',
         'postal_code', 'city']
+
+        
         
