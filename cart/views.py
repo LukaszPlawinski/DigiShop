18f2bdfe8ced5eec01c444260ba11a1310da6ca8
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-07 16:20:57 +0000

    navbar moved to base.html

--- cart/views.py
+++ cart/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from shop.models import Product
+from shop.models import Product, Category
 from .cart import Cart
 from .forms import CartAddProductForm
 
 def cart_detail(request):
     cart = Cart(request)
+    categories = Category.objects.all()
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
                                         initial={'quantity': item['quantity'],
                                         'update': True})
-    return render(request, 'cart_detail.html', {'cart': cart})
+    return render(request, 'cart_detail.html', 
+                {'cart': cart, 
+                'categories': categories})
     
 def cart_remove(request, product_id):
     cart = Cart(request)
--- search/views.py
+++ search/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from shop.models import Product
+from shop.models import Product, Category
 
 
 # Create your views here.
 
 def search (request):
+    category = None
+    categories = Category.objects.all()
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "list.html", {"products": products})
+    return render(request, "list.html", {"products": products, 'categories': categories})
