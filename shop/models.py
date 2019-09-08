20518164c29869becb6f7a94179b837fb6476499
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-08 14:53:25 +0000

    blank= True

--- shop/models.py
+++ shop/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
                               blank=True)
     description = models.TextField(blank=True)
     price = models.IntegerField(blank="False")
-    old_price = models.IntegerField(blank="False", default=1)
+    old_price = models.IntegerField(blank=True, default="")
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
