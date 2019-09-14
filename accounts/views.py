f1c2de05a229a82283b53e0b5e54d4a97636d63a
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-14 06:57:41 +0000

    comment out the code

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,13 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from shop.views import product_list
 from shop.models import Category
+from django.contrib.auth.decorators import login_required
 
+# Importing coategories list which is used in Nav
 categories = Category.objects.all()
 
-
+''' When user is logged in redirect him to login page,
+if method is Post and login form valid then login'''
 def login(request):
     if request.user.is_authenticated:
         return redirect(reverse('shop:product_list'))
@@ -25,24 +28,31 @@ def login(request):
         login_form = user_login_form()
     return render(request, 'login.html', {'login_form': login_form, 'categories':categories})
     
-
+    
+'''Logout function'''
+@login_required
 def logout(request):
     auth.logout(request)
     return redirect(reverse('shop:product_list'))
     
+    
+'''Registration Function'''
 def registration(request):
     if request.user.is_authenticated:
         return redirect(reverse('shop:product_list'))
+        
     if request.method == "POST":
         registration_form = register_user_form(request.POST)
+        
         if registration_form.is_valid():
             registration_form.save()
-            
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password1'])
+                                     
             if user:
                 auth.login(user=user, request=request)
                 return redirect(reverse('shop:product_list'))
+                
     else:
         registration_form = register_user_form()
     return render(request, 'registration.html', {
