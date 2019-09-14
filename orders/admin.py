cc1c4590d3a5e45e10ed12eb46ef53d041c2627b
Ubuntu <ubuntu@ip-172-31-87-170.ec2.internal>
2019-09-14 11:14:48 +0000

    clean blank spaces

--- orders/admin.py
+++ orders/admin.py
@@ -7,7 +7,7 @@ class OrderItemInline(admin.TabularInline):
     
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email',
+    list_display = ['id','user','total', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
